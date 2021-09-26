a10fab7cdab5ddfa5192ed67d42129d192b96c5d
Muhammad Zunaib Afzal <36011641+zunaibafzal@users.noreply.github.com>
2021-09-26 18:44:01 +0500

    26-09-2021
    26-09-2021 today class

---
+++ 26-09-2021/src/Components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css';
+
+function Contact() {
+    return (
+        <div className='main'>
+            <h1>Contact Page</h1>
+        </div>
+    )
+}
+
+export default Contact
